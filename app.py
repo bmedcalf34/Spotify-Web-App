524914d9bfb3c5da417e24f41e6397ed032eefbb
bmedcalf34 <77631274+bmedcalf34@users.noreply.github.com>
2022-01-26 10:55:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,51 +6,137 @@ import spotipy.oauth2 as oauth2
 import random
 import flask
 import requests
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
 
 load_dotenv(find_dotenv())
 app = flask.Flask(__name__)
 
-AUTH_URL = 'https://accounts.spotify.com/api/token'
+AUTH_URL = "https://accounts.spotify.com/api/token"
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-artists = ['spotify:artist:4pejUc4iciQfgdX6OKulQn', 'spotify:artist:1Yox196W7bzVNZI7RBaPnf', 'spotify:artist:3HCpwNmFp2rvjkdjTs4uxs'] 
-#QOTSA, MEGADETH, KYUSS
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+app.secret_key = bytes(os.getenv("SECRET_KEY"), "utf-8")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    artists = db.Column(db.ARRAY(db.String(64)), nullable=True)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+    def __repr__(self):
+        return self.username
+
+
+@login_manager.user_loader
+def load_user(username):
+    return Task.query.filter_by(username=username).first()
+
+
+# db.create_all()
+# db.drop_all()
+
 
 @app.route("/")
 def index():
+    return flask.render_template("login.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user_name = flask.request.form["username"]
+        if Task.query.filter_by(username=user_name).first() == None:
+            return flask.redirect("/signup")
+        else:
+            login_user(Task.query.filter_by(username=user_name).first())
+            return flask.redirect("/add_artists")
+    return flask.redirect("/add_artists")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        user_name = flask.request.form.get("new_username")
+        task = Task(username=user_name, artists=[])
+        db.session.add(task)
+        db.session.commit()
+        return flask.render_template("index.html")
+    return flask.render_template("signup.html")
+
+
+@app.route("/add_artists", methods=["GET", "POST"])
+def add_artists():
+    if request.method == "POST":
+        username = current_user.username
+        user = Task.query.filter_by(username=username).first()
+        _ids = list(user.artists)
+        _ids.append(flask.request.form.get("new_artist"))
+        user.artists = _ids
+        print(user.artists, "hi")
+        db.session.commit()
+        return flask.redirect("/artists")
+    return flask.render_template("index.html")
+
+
+@app.route("/artists")
+def artists():
     song_name, artist_name, preview, cover = pick_artist_and_song()
-    return flask.render_template("index.html", song_name = song_name, artist_name = artist_name, preview = preview, cover = cover)
-    #render template here
+    return flask.render_template(
+        "index.html",
+        song_name=song_name,
+        artist_name=artist_name,
+        preview=preview,
+        cover=cover,
+        current_user=current_user,
+    )
+
 
 def pick_artist_and_song():
-    num_list = [0, 1, 2]
-    chosen_artist = random.choice(num_list) #create a list 1-3, choose random number, and assign that number as the artist
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    Task.query.filter_by(username=current_user.username).first().artists
+    artists = Task.query.filter_by(username=current_user.username).first().artists
+    pick = random.choice(artists)
 
     song_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     chosen_song = random.choice(song_list)
 
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    results = sp.artist_top_tracks(pick)
+    track = results["tracks"][chosen_song - 1]
+    artist_name = track["album"]["artists"][0]["name"]
+    song_name = track["name"]
+    preview = str(track["preview_url"])
+    cover = track["album"]["images"][0]["url"]
+
+    return song_name, artist_name, preview, cover
 
 
-    results = sp.artist_top_tracks(artists[chosen_artist]) #the results wil be assigned to the random artist chosen from the list above
-
-    track = results['tracks'][chosen_song - 1]
-    artist_name = track['album']['artists'][0]['name']
-    song_name = track['name']
-    preview = str(track['preview_url'])
-    cover = track['album']['images'][0]['url']
-    print('track: ' + track['name'])
-    print('audio: ' + str(track['preview_url']))
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
-    #above function does what it appears to do, but for some reason only works all the way for kyuss
-
-    return song_name, artist_name,  preview, cover
-
-
-app.run(
-    host = '0.0.0.0',
-    port =int(os.getenv("PORT", 8080))
-)
+app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
--- readme.md
+++ readme.md
@@ -17,3 +17,23 @@
 ## Two Technical Issues and How I resolved Them
 1. The first technicl issue I had was getting the verification for spotipy to work properly. For a while my code was throwing an error like "API KEY not present" or something similar. What I did tp fix this issue was read the spotipy docs, strangely enough. Going through the detailed processes there on how to get auth access set me straight. Ironically enough, going straight to Stack Overflow was what messed me up and almost made me start over, as none of the answers fixed anything.
 2. The second set of issues I ran into was anything relating to getting the data from app.py into html via flask. My process was asking my friend Brandon for help and him walking me through a lot of the issues. Some of these included splitting up the code into functions and then calling the functions from the flask function, as well as initializing the variables that I wanted to transfer to index.html, like preview, song_name, artist, and so on. I'd just like to shout out Brandon and if you can give him like 5% extra credit for helping me so much that would be awesome.
+
+
+# Bennett Medcalf Project 1 Milestone 2
+
+
+
+## Set Up For Local Use
+1. Only thing you may need to run locally is SQLAlchemy, flask-login. You should have spotipy and other libraries from the first milestone.
+2. A note about how this app functions: when entering an invalid username, you will be redirected to the signup form. From there, enter your desired username and hit submit to be redirected back to login, where you can input your username to login.
+3. Another note: the app DOES save favorite artists across sessions, what you have to do is hit the "Submit" button at the bottom of the "artists" page to refresh the page and display a song.
+
+## Two Known Problems and How I would Address Them
+1. My first known problem is that an incorrect login does not flash an error. Instead, it simply redirects to the sign up page for you to enter 
+a new username. If I had more time, I would add a flash message saying that the password is incorrect and that the user is being
+redirected to the sign up screen.
+2. A second known problem is that my app accepts invalid Spotify URIs. If I had more time, I would add a conditional statement that checks the Spotify endpoint on whether it returns a top ten tracks for the given URI. If it returns "None" or "null", then it will flash a warning that the entered URI is incorrect and that it was not accepted into the database.
+
+## Two Technical Issues and How I Resolved Them
+1. The first technical issue I had was with an error along the lines of "id not found" at the Spotify API endpoint. The root of this error was an incorrect URI input that was not found when checking Spotify. My solution to this was actually quite silly, as the cause of the problem was me getting the URI from a playlist I created instead of from the artist. Making sure to go to the artist's Spotify page and get the correct URI wa the solution to this problem.
+2. A second technical issue I ran into was when I input a new username into the database. because the username was new, there were no artist IDs associated with it in the database. The solution to this was also quite simple, but tricky at the same time. What I did to fix this issue was switch around my flask redirects in the different functions. This was mostly trial and error as well as tracing through my code and seeing what cases led to what redirects. The root cause of this error was that upon signup, the user was redirected to the "artists" page, which displays saved artists, instead of the "add_artists" function which adds artists to new users.
